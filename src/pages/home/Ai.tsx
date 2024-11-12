82593c2736cf9739bf231e06852642b5a2965a07
erain <enesyagmur99@gmail.com>
2024-11-12 18:56:28 +0300

    ai için arka plan responsive değişiklik yapma

--- src/pages/home/Ai.tsx
+++ src/pages/home/Ai.tsx
@@ -7,7 +7,7 @@ const Ai = () => {
 
   return (
     <div className="w-full lg:w-11/12 min-h-svh lg:h-[600px] relative flex items-center justify-center">
-      <div className="w-full h-full bg-green_custom rounded-lg opacity-50 absolute"></div>
+      <div className="w-full h-full lg:h-5/6 bg-green_custom rounded-lg opacity-50 absolute"></div>
 
       <div className="h-5/6 lg:h-[500px] rounded-lg w-full  lg:w-11/12 bg-green_custom z-10 flex flex-col items-center justify-center">
         {list.length ? (
